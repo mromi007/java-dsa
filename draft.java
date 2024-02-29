42fa2e9176f16eb9bafaed21c463bd7ee9224305
Om Ganjale <ganjaleom14@gmail.com>
2024-02-29 12:02:06 +0530

    vs upload

---
+++ draft.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class draft {
+  /*public static void main(String []args){
+    System.out.println("Hello World");
+  }*/
+
+
+
+
+  
+// income tax calculator
+  /*public static void main(String[] args) {
+    System.out.println("Enter your income");
+    Scanner sc = new Scanner(System.in);
+    int income = sc.nextInt();
+    int tax;
+    if(income < 50000){
+      tax = 0;
+    }
+    else if(income >= 50000 && income <100000){
+      tax = (int) (income * 0.2);
+    }
+    else{
+      tax = (int) (income * 0.3);
+    }
+    System.out.println("Tax = " + tax);
+  }*/
+
+
+
+
+
+  // print the largest number
+  /*public static void main(String[] args) {
+    System.out.println("a");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println("b");
+    Scanner bc = new Scanner(System.in);
+    int b = bc.nextInt();
+    System.out.println("c");
+    Scanner mc = new Scanner(System.in);
+    int c = mc.nextInt();
+
+    if((a>=b) && (a>=c)){
+      System.out.println("a is greater");
+    }
+    else if(b>=c){
+      System.out.println("b is greater");
+    }
+    else{
+      System.out.println("c is greater");
+    }
+  }*/
+
+
+
+
+  
+}
+
+  
